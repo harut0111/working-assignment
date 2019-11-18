f48b4741eafc8a5d18460ff201c892152edb1216
Harutyun Muqoyan <hmuqoyan@gmail.com>
2019-11-18 15:57:49 +0400

    added work data table

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ function App() {
     <div className="App">
       <LeftMenu />
       <Main />
-
     </div>
   );
 }
--- src/Components/Main/index.jsx
+++ src/Components/Main/index.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { FaBell, FaUserCircle } from 'react-icons/fa';
+import WorkDataTable from './Table';
 
 const index = () => {
     return (
         <div className='main'>
             <div className='main-content'>
-                {/* part 1 */} 
+
+                {/* part 1 ---- main-header -----*/} 
                 <div className='main-header'>
                     <div className='main-header-text'>
                         <h1>Рабочие задания</h1>
@@ -15,12 +17,35 @@ const index = () => {
                         <span><FaUserCircle /></span>
                     </div>
                 </div>
-                {/* part 2 */}
-
-                <div className='adding '>
-
-                </div>
                 
+                {/* part 2 ----- adding-work -----*/}
+                <div className='main-addingWork'>
+                    <div className='main-addingWork-content'>
+                        <div>div 1</div>
+                        <div>div 2</div>
+                        <div>div 3</div>
+                        <div>div 4</div>
+                        <div>div 5</div>
+                    </div>
+                </div>
+
+                {/* part 3 ---- data-filter */}
+                <div className='main-filter'>
+                    <div className='main-filter-content'>
+                        <div>div 1</div>
+                        <div>div 2</div>
+                        <div>div 3</div>
+                        <div>div 4</div>
+                        <div>div 5</div>
+                    </div>
+                </div>
+
+                {/* part 4 ---- table */}
+                <div className='main-table'>
+                    <div className='main-table-content'>
+                        <WorkDataTable />
+                    </div>
+                </div>
             </div>
         </div>
     )
