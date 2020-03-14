c74f8d02eb9a1a42481cb4cf27b46c1ae2640896
Harut Muqoyan <hmuqoyan@gmail.com>
2020-03-14 22:16:16 +0400

    updated

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
-import React from 'react';
-import './App.scss';
-import LeftMenu from './Components/LeftMenu';
-import Main from './Components/Main';
-import { StateProvider } from './Context';
-import { initialState, reducer } from './Context/reducer';
-
+import React from "react";
+import "./App.scss";
+import LeftMenu from "./Components/LeftMenu";
+import Main from "./Components/Main";
+import { StateProvider } from "./Context";
+import { initialState, reducer } from "./Context/reducer";
 
 function App() {
   return (
     <div className="App">
-        <StateProvider initialState={initialState} reducer={reducer}>
-            <LeftMenu />
-            <Main />
-        </StateProvider>
+      <StateProvider initialState={initialState} reducer={reducer}>
+        <LeftMenu />
+        <Main />
+      </StateProvider>
     </div>
   );
 }
